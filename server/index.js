73f84be33c536738626ba277601a90a8a9807964
Tom Germeau <tom.germeau@gmail.com>
2016-09-21 20:57:53 -0400

    accept files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const url = require('url');
+const util = require('util');
 const fs = require('fs');
+const path = require('path');
+const formidable = require('formidable');
 const port = 3001;
 const app = express();
 const bodyParser = require('body-parser');
-
-function getProjectPath(query) {
-  return './projects/' + parseInt(query.id.replace(/\./g,''), 10);
-}
+const mkdirp = require('mkdirp');
+const projectsDir = './projects/';
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,10 +16,36 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.post('/upload', bodyParser.text({ type: 'text/plain' }), function(req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    // console.log(util.inspect({fields: fields, files: files}));
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received upload:\n\n');
+    res.end(util.inspect({fields: fields, files: files}));
+
+    var documentId = String(fields.documentId);
+    if (files.image) {
+      var file = files.image;
+      mkdirp(path.join(projectsDir, documentId, 'images'), function() {
+        fs.rename(file.path, path.join(projectsDir, documentId, 'images', file.name));
+      });
+    }
+    if (fields.json) {
+      console.log('got json');
+      mkdirp(path.join(projectsDir, documentId), function() {
+        let stream = fs.createWriteStream(path.join(projectsDir, documentId, 'layers.json'));
+        stream.write(fields.json);
+        stream.end();
+      });
+    }
+  });
+});
+
 app.get('/app.coffee', function(req, res) {
   try {
-    fs.createReadStream(getProjectPath(req.query)).on('open', function() {
-      console.log('reading coffee', getProjectPath(req.query))
+    var coffeePath = path.join(projectsDir, parseInt(req.query, 10), 'app.coffee');
+    fs.createReadStream(coffeePath).on('open', function() {
       this.pipe(res);
     }).on('error', function() {
       res.send('')
@@ -33,8 +60,8 @@ app.post('/app.coffee', bodyParser.text({ type: 'text/plain' }), function(req, r
   if(req.body.length === 0) {
     return;
   }
-
-  let stream = fs.createWriteStream(getProjectPath(req.query));
+  var coffeePath = path.join(projectsDir, parseInt(req.query, 10), 'app.coffee');
+  let stream = fs.createWriteStream(coffeePath));
   stream.write(req.body);
   stream.end();
   res.status(200).send('thanks!');
