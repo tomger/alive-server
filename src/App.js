3018df03359f266f7d6c8eccbad6738a03d65d80
Tom Germeau <tom.germeau@gmail.com>
2016-10-02 17:22:55 -0400

    drag and drop Views

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
+import { DragSource, DropTarget,    DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
 import ace from 'brace';
 import 'brace/mode/coffee';
 import 'brace/theme/monokai';
@@ -7,6 +10,8 @@ import 'brace/ext/language_tools';
 
 import './App.css';
 import words from './words';
+
+const VIEW_DRAG_TYPE = 'view';
 
 function parseCode (code) {
   // Really really dumb parsing of the view tree code
@@ -126,6 +131,23 @@ class App extends Component {
     this.editor.focus();
   }
 
+  insertViewCode(view) {
+    let snippet = `\nviews.pushIn layers.${view}`;
+
+    let code = this.codeTree[this.state.selectedView] || '';
+    code += snippet;
+    this.codeTree[this.state.selectedView] = code;
+    this.setState({ code });
+
+    let lines = code.split('\n');
+    let position = lines.length;
+    let cursor = lines[lines.length - 1].length;
+    this.editor.resize(true);
+    this.editor.scrollToLine(position, true, true, function () {});
+    this.editor.gotoLine(position, cursor, true);
+    this.editor.focus();
+  }
+
   focusView(view) {
     this.setState({
       selectedView: view,
@@ -187,9 +209,9 @@ class App extends Component {
       this.setState({
         code: this.codeTree[this.state.selectedView]
       });
-      var undo_manager = this.refs.ace.editor.getSession().getUndoManager();
-      undo_manager.reset();
-      this.refs.ace.editor.getSession().setUndoManager(undo_manager);
+      // var undo_manager = this.refs.ace.editor.getSession().getUndoManager();
+      // undo_manager.reset();
+      // this.refs.ace.editor.getSession().setUndoManager(undo_manager);
     });
   }
 
@@ -248,19 +270,42 @@ class App extends Component {
             backgroundImage: `url(/imported/${this.documentId}@2x/images/${artboard.objectId}.png)`
           };
           return (
-            <div key={artboard.name}
-              className={['Thumbnail', this.state.selectedView === artboard.name ? 'Thumbnail--selected' : undefined].join(' ')}
-              onClick={e => this.onSelectView(artboard)}>
-              <div className="Thumbnail__image" style={ style }></div>
-              <div className="Thumbnail__title">
-                {artboard.name}
-              </div>
-            </div>
+            <DraggableThumbnail
+              key={artboard.name}
+              name={artboard.name}
+              selected={this.state.selectedView === artboard.name}
+              style={style}
+              onClick={e => this.onSelectView(artboard)}
+              />
           )
         }
       );
     }
 
+    let editor = this.props.connectDropTarget(
+      <div style={{ display: 'flex', flex: '1 1', flexDirection: 'column'}}>
+        <AceEditor
+         className="editor"
+         height="500px"
+         width="auto"
+         mode="coffee"
+         ref="ace"
+         theme="monokai"
+         fontSize={14}
+         value={this.state.code}
+         onChange={e => this.onChange(e)}
+         commands={this.commands}
+         editorProps={{$blockScrolling: true}}
+         onLoad={(editor) => {
+           this.editor = editor;
+           editor.focus();
+           editor.getSession().setUseWrapMode(true);
+           editor.setOptions({
+             enableLiveAutocompletion: true
+           });
+         }}/>
+      </div>
+    );
 
     return (
       <div className="App">
@@ -273,39 +318,92 @@ class App extends Component {
             overflowY: 'auto'}}>
             {pages}
           </div>
-          <div style={{ display: 'flex', flex: '1 1', flexDirection: 'column'}}>
-            <AceEditor
-               className="editor"
-               height="500px"
-               width="auto"
-               mode="coffee"
-               ref="ace"
-               theme="monokai"
-               fontSize={14}
-               value={this.state.code}
-               onChange={e => this.onChange(e)}
-               commands={this.commands}
-               editorProps={{$blockScrolling: true}}
-               onLoad={(editor) => {
-                 this.editor = editor;
-                 editor.focus();
-                 editor.getSession().setUseWrapMode(true);
-                 editor.setOptions({
-                  //  enableBasicAutocompletion: true,
-                   enableLiveAutocompletion: true
-                 });
-               }}
-             />
-           {/* <div className="statusbar">
-              <a className="" href="https://framerjs.com/docs/" target="_blank">Framer Docs</a>
-              <div className="statusbar-button" title="Cmd + Shift + S" onClick={ e => this.saveAndReload() }>Save &amp; Run</div>
-           </div> */}
-           </div>
-           <iframe ref="preview" className="preview" src={`/framer.html?id=${this.documentId}`}></iframe>
+          {editor}
+          <iframe ref="preview" className="preview" src={`/framer.html?id=${this.documentId}`}></iframe>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+const thumbnailSource = {
+  beginDrag(props) {
+    return {
+      name: props.name
+    };
+  }
+};
+
+function collectThumbnail(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+function collectEditor(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget()
+  };
+}
+
+
+var appDropTarget = {
+  canDrop: function (props, monitor) {
+    // console.log('canDrop', props.position)
+    // You can disallow drop based on props or item
+    // var item = monitor.getItem();
+    // return canMakeChessMove(item.fromPosition, props.position);
+    return true;
+  },
+
+  hover: function (props, monitor, component) {
+    // console.log('hover')
+    // This is fired very often and lets you perform side effects
+    // in response to the hover. You can't handle enter and leave
+    // here—if you need them, put monitor.isOver() into collect() so you
+    // can just use componentWillReceiveProps() to handle enter/leave.
+
+    // You can access the coordinates if you need them
+    // var clientOffset = monitor.getClientOffset();
+    // var componentRect = findDOMNode(component).getBoundingClientRect();
+
+    // You can check whether we're over a nested drop target
+    // var isJustOverThisOne = monitor.isOver({ shallow: true });
+
+    // You will receive hover() even for items for which canDrop() is false
+    // var canDrop = monitor.canDrop();
+  },
+
+  drop: function (props, monitor, component) {
+    let item = monitor.getItem();
+    let name = item.name;
+    component.insertViewCode(name);
+  }
+};
+
+class Thumbnail extends Component {
+  render() {
+    const { isDragging, connectDragSource } = this.props;
+    let style = {};
+    if (isDragging) {
+      style.opacity = 0.5;
+    }
+    return connectDragSource(
+      <div
+        className={['Thumbnail', this.props.selected ? 'Thumbnail--selected' : undefined].join(' ')}
+        onClick={this.props.onClick}
+        style={style}>
+        <div className="Thumbnail__image" style={ this.props.style }></div>
+        <div className="Thumbnail__title">
+          {this.props.name}
+        </div>
+      </div>
+    );
+  }
+}
+
+var DraggableThumbnail = DragSource(VIEW_DRAG_TYPE, thumbnailSource, collectThumbnail)(Thumbnail);
+
+let droppableApp = DropTarget(VIEW_DRAG_TYPE, appDropTarget, collectEditor)(App);
+export default DragDropContext(HTML5Backend)(droppableApp);
